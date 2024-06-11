258614c52eb8c22cb43f9b38839a24ec1cb7c43b
JBKIM <drv98@naver.com>
2024-06-11 15:29:26 +0900

    오버레이 이미지와 글

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,8 +4,12 @@ const MovieList = (props) => {
   return (
     <>
       {props.movies.map((m) => (
-        <div className="d-flex m-3" key={m.imdbID}>
+        <div className="image-container d-flex m-3" key={m.imdbID}>
           <img src={m.Poster} alt="movie"></img>
+          <div className="overlay d-flex align-items-center justify-content-center">
+            <span className="me-2">선호작 추가</span>
+            <span>❤️</span>
+          </div>
         </div>
       ))}
     </>
